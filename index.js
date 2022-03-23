2aaa2fa101e7cba502d583f6dbce79ffad8690b4
Sumit Kumar Bighaniya <sb78639@gmail.com>
2022-03-23 13:43:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
   console.log("Listening on port " + PORT);
